057d0d7cc230b5e6104cc5e928d1ac359c436189
z_andr <andr.zakharevich@gmail.com>
2020-08-31 22:59:15 +0300

    update 5

--- azakharevich/octonion/AQA_lessons/hometask1-5.py
+++ azakharevich/octonion/AQA_lessons/hometask1-5.py
@@ -1,10 +1,27 @@
 print("Требуется отсортировать массив по возрастанию. (используя поиск максимального ИЛИ минимального элемента)")
 
-arr = [3, 1, 10, 1, 2, 10, 5]
+arr = [3, 1, 10, 1, 2, 10, 5, 5, 5, 8]
 print('Было: ', arr)
 
-i = 1
+i = 0
 
+while i < len(arr):
+    buf = i
+    j = i
+    while j < len(arr):
+        if arr[j] <= arr[buf]:
+            buf = j
+            j += 1
+        j += 1
+    tmp = arr[i]
+    arr[i] = arr[buf]
+    arr[buf] = tmp
+    i += 1
+
+print('Стало: ', arr)
+
+'''
+Не такой уж крутой код, но тоже ничего
 while i < len(arr):
     while arr[i] < arr[i-1]:
         tmp = arr[i]
@@ -15,9 +32,7 @@ while i < len(arr):
     i += 1
 
 print('Стало: ', arr)
-
-
-
+'''
 
 
 '''
