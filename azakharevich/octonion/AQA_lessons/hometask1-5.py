cd168035371cf9e1c986dff4fda577c2617de445
z_andr <andr.zakharevich@gmail.com>
2020-08-31 19:23:39 +0300

    update 5

--- azakharevich/octonion/AQA_lessons/hometask1-5.py
+++ azakharevich/octonion/AQA_lessons/hometask1-5.py
@@ -1,13 +1,34 @@
 print("Требуется отсортировать массив по возрастанию. (используя поиск максимального ИЛИ минимального элемента)")
 
+arr = [3, 1, 10, 1, 2, 10, 5]
+print('Было: ', arr)
+
+i = 1
+
+while i < len(arr):
+    while arr[i] < arr[i-1]:
+        tmp = arr[i]
+        arr[i] = arr[i-1]
+        arr[i-1] = tmp
+        if i > 1:
+            i -= 1
+    i += 1
+
+print('Стало: ', arr)
+
+
+
+
+
+'''
+Крутой питонский код
+
 arr1 = [3, 1, 10, 2, 5]
 arr2 = []
 
-i = 0
-
 print('Было: ', arr1)
 
-while i < len(arr1):
+while 0 < len(arr1):
     j = 0
     while j < len(arr1):
         if arr1[j] == min(arr1):
@@ -18,3 +39,4 @@ while i < len(arr1):
             j += 1
 
 print('Стало: ', arr2)
+'''
