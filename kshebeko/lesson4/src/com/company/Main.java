4cd0b1002b9e1a6d97e19e76e1b2b590003a476f
Katerina_Shebeko <katerinashebeko@gmail.com>
2020-09-02 09:03:14 +0300

    lesson4

---
+++ kshebeko/lesson4/src/com/company/Main.java
@@ -0,0 +1,127 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Определить возможность существования треугольника по сторонам
+        // Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
+        //Дано: a, b, c – стороны предполагаемого треугольника.
+      /*int a = 67, b = 4, c = 4;
+     if (a + b > c & a + c > b & b + c > a){
+         System.out.println("Треугольник существует");
+     }
+     else {
+         System.out.println("Треугольник не существует");
+     }
+
+*/
+        //Возведение числа в степень
+        //Дано: n - число, d - степень
+      /*  int n = 4;
+        int b = 5;
+        int res = 1;
+        for (int i = 1; i <= b; i++){
+            res = res * n;
+            System.out.println("Возведение числа в степень равно = " + res);
+        }
+
+       */
+
+        // Вывести делители числа
+        //Для натурального числа n вывести все делители, кроме единицы и самого числа.
+    /*int n = 90;
+    int del = 1;
+    for(int i = 2; i < n; i++){
+        if (n % i == 0){
+            System.out.println("Целое число:" + i);
+        }
+
+    }
+
+     */
+        //Сумма положительных элементов массива
+        //Найти сумму положительных элементов массива.
+        /*int[] arr = {9, -6, 3, 5, -6, 7, 9};
+        System.out.println(Arrays.toString(arr));
+int sum = 0;
+         for(int i = 0; i < arr.length; i++){
+             if (arr[i] > 0){
+               sum = sum + arr[i]; // 9
+               System.out.println("Сумма = " + sum);
+             }
+         }
+
+         */
+//Вывести элементы массива, которые больше среднего арифметического
+//Дан одномерный массив. Найти среднее арифметическое его элементов. Вывести на экран только те элементы массива, которые больше найденного среднего арифметического.
+        /*int [] arr = {5, 4, 8, 0, 9, 4, 1, 3};
+        System.out.println(Arrays.toString(arr));
+        int sum = 0;
+        int arith = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма = " + sum);
+        arith = sum / arr.length;
+        System.out.println("Сред ариф = " + arith);
+        for (int i = 0; i < arr.length; i++){
+            if ( arith < arr[i]){
+                System.out.println("Элеиенты массива больше сред. ариф :" + arr[i]);
+            }
+
+        }
+*/
+        //Есть ли в массиве одинаковые элементы
+        //Дан массив целых чисел. Проверить, есть ли в нем одинаковые элементы.
+       /* int[] arr = {6, 9, 6, 6, 8, 8, 7};
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Одинаковые элементы: " + arr[i]); // 9; 6;8
+                }
+            }
+
+        }
+*/
+        //*Сортировка выбором
+        //Требуется отсортировать массив по возрастанию. (используя поиск максимального ИЛИ минимального элемента)
+        /*int[] arr = {4, 7, 9, 10, 15, 7, 0, 4, 3};
+        for(int i = 0; i < arr.length; i++){
+            int min = arr[i];
+            int imin = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < min){
+                   min = arr[j];
+                   imin = j;
+                   System.out.println("Минимальный элемент:" + min);
+                }
+            }
+
+            arr[imin] = arr[i];
+            arr[i] = min;
+            System.out.println(Arrays.toString(arr));
+        }
+
+*/
+        //Комбинации из трех чисел, дающие в сумме заданное число
+        //Написать программу, которая находит все комбинации из трех чисел до определенного предела, которые в сумме дают другое число.
+        //Пример:
+        //Дано: Предел для перебираемых чисел: 5, Искомая сумма: 10
+        //1 4 5, 1 5 4, 2 3 5, ...
+        int sum = 10;
+        int pre = 5;
+        int a = 0;
+        int b = 0;
+        while ( a + b < 5) {
+
+            sum = pre*2 + a + b;
+            a = sum/2 - pre%2;
+            b = pre - a;
+            System.out.println(" Числа: " + a + " и " + b + " и " + pre);
+
+
+        }
+    }
+}
