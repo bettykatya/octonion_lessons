822d5771376263a06304634cf7273eccbb32707c
Alena_Mi <lenalikestesting@gmail.com>
2020-10-06 23:38:50 +0300

    exception In progress not work

---
+++ alena/src/com/octonion/lesson2/EmptyCandyNameException.java
@@ -0,0 +1,4 @@
+package com.octonion.lesson2;
+
+public class EmptyCandyNameException {
+}
--- alena/src/com/octonion/lesson2/Main.java
+++ alena/src/com/octonion/lesson2/Main.java
@@ -10,44 +10,75 @@ package com.octonion.lesson2;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EmptyCandyNameException {
 
 
-        Chokolate bounty = new Chokolate("Bounty", 7, 14);
+        Chokolate bounty = new Chokolate("", 7, 14);
         Chokolate mars = new Chokolate("Mars", 10, 19);
         Ledency barbariska = new Ledency("Barbariska", 4, 6);
         Solty lakrica = new Solty("Lakrica", 4, 5);
 
+        Chokolate emptyCandyName = new Chokolate("", 7, 14);
+
         Present present = new Present();
-        present.addCandy(lakrica); //5
-        present.addCandy(bounty); //14
-        present.addCandy(lakrica); //5
-        present.addCandy(barbariska); //6
-        present.addCandy(mars); //19
-        present.addCandy(barbariska); //6
-        present.addCandy(bounty); //14
+        try {
+            present.addCandy(lakrica);
+            present.getCandies(emptyCandyName).get(0);
+            try {
+                present.addCandy();
+                throw new ArrayIndexOutOfBoundsException();
+            } catch (NullPointerException npe) {
+                throw new EmptyCandyNameException();
+            }
 
-        System.out.println();
-        System.out.println("стоимость - " + present.getPrice());
-        System.out.println("Вес - " + present.getWeight());
-        System.out.println();
-
-
-        String cnd = "Total sugar in range - ";
-        ArrayList<NewYearCandy> calculateTotalSugar = present.calculateTotalSugar(30, 60);
-        for (int i = 0; i < calculateTotalSugar.size(); i++) {
-            NewYearCandy candy = calculateTotalSugar.get(i);
-            cnd += " " + candy.getName();
+        } catch (EmptyCandyNameException e) {
+            System.out.println("exception1");
+        } catch (NullPointerException npe) {
+            System.out.println("exception2");
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            System.out.println("exception3");
+        } catch (Exception exception) {
+            System.out.println("exception4");
         }
-        System.out.println();
-        System.out.println(cnd);
-        System.out.println();
+//        present.getCandies().get(2);
+//        present.addCandy(lakrica); //5
+//        present.addCandy(bounty); //14
+//        present.addCandy(lakrica); //5
+//        present.addCandy(barbariska); //6
+//        present.addCandy(mars); //19
+//        present.addCandy(barbariska); //6
+//        present.addCandy(bounty); //14
 
 
-        present.printCandies();
-        present.sortCandies();
-        present.printCandies();
-        System.out.println();
+//        try {
+//            present.addCandy(emptyCandyName);
+//            throw new EmptyCandyNameException();
+//        } catch (EmptyCandyNameException e) {
+//            System.out.println();
+//            System.out.println("empty candy name! Achtung!");
+//        }
+
+//        System.out.println();
+//        System.out.println("стоимость - " + present.getPrice());
+//        System.out.println("Вес - " + present.getWeight());
+//        System.out.println();
+//
+//
+//        String cnd = "Total sugar in range - ";
+//        ArrayList<NewYearCandy> calculateTotalSugar = present.calculateTotalSugar(30, 60);
+//        for (int i = 0; i < calculateTotalSugar.size(); i++) {
+//            NewYearCandy candy = calculateTotalSugar.get(i);
+//            cnd += " " + candy.getName();
+//        }
+//        System.out.println();
+//        System.out.println(cnd);
+//        System.out.println();
+//
+//
+//        present.printCandies();
+//        present.sortCandies();
+//        present.printCandies();
+//        System.out.println();
 
 
     }
