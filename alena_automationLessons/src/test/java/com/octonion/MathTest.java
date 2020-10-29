0331820ac58d53cac5f288102b461849e50d8f58
Alena_Mi <lenalikestesting@gmail.com>
2020-10-30 01:22:41 +0300

    task-1-am-maven: i created new project with new pom file. with old there were trable - cant create class  -  In Progress

---
+++ alena_automationLessons/src/test/java/com/octonion/MathTest.java
@@ -0,0 +1,25 @@
+package com.octonion;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class MathTest {
+
+    public Math math;
+
+
+
+    @BeforeClass
+
+
+    @AfterMethod
+
+    @Test
+
+
+    @DataProvider
+}
