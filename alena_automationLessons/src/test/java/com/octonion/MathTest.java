93c9702561a52bd671f1a28e171ea1bdb58c2c4b
Alena_Mi <lenalikestesting@gmail.com>
2020-11-01 22:57:46 +0300

    task-1-am-maven-project: In progress Math tests

--- alena_automationLessons/src/test/java/com/octonion/MathTest.java
+++ alena_automationLessons/src/test/java/com/octonion/MathTest.java
@@ -12,14 +12,35 @@ public class MathTest {
     public Math math;
 
 
-
-    @BeforeClass
-
-
-    @AfterMethod
+    @Test
+    public void testSoftAssert() {
+        Assert.assertEquals(Math.sqrt(4), 2);
+    }
 
     @Test
+    public void testSoftAssert2() {
+        Assert.assertEquals(Math.sqrt(6), 1);
+    }
+
+    @DataProvider(name = "testWithDataProvider")
+    public Object[][] createData1() {
+        return new Object[][]{
+                {2, 2, 4},
+                {1, 1, 1},
+                {3, 2, 9}
+        };
+    }
 
 
-    @DataProvider
+    @Test(dataProvider = "testWithDataProvider")
+    public void checkPowerOfNumber(int a, int b, int res) {
+        Assert.assertEquals(Math.pow(a, b), res);
+    }
+
+    @AfterMethod
+    public void checkPowerOfNumber() {
+        System.out.println("Aftermethod");
+    }
+
+
 }
