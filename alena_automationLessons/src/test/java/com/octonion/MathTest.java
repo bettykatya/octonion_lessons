5c22ffec140b948835339b8261b292508d5431ce
Alena_Mi <lenalikestesting@gmail.com>
2020-11-08 16:15:14 +0300

    task-1-maven-project: InProgress: Left expect exeption

--- alena_automationLessons/src/test/java/com/octonion/MathTest.java
+++ alena_automationLessons/src/test/java/com/octonion/MathTest.java
@@ -6,16 +6,10 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
 
 public class MathTest {
 
-
-
-
-    @Test(groups = {"critical","positive"})
+    @Test(groups = {"critical", "positive"})
     public void testSoftAssert() {
         Assert.assertEquals(Math.sqrt(4), 1);
     }
@@ -39,16 +33,34 @@ public class MathTest {
         };
     }
 
-
     @Test(dataProvider = "testWithDataProvider", groups = "positiveChoices")
     public void checkPowerOfNumber(int a, int b, int res) {
         Assert.assertEquals(Math.pow(a, b), res);
+    }
+
+    @Test(dependsOnMethods = "roundNumber1", groups = "round")
+    public void roundNumber3() {
+        System.out.println("roundNumber3");
+        Assert.assertEquals(Math.round(2.533), 3, "2.533 = 3");
+    }
+
+    @Test(dependsOnMethods = "roundNumber2", alwaysRun = true, groups = "round")
+    public void roundNumber1() {
+        System.out.println("roundNumber1");
+        Assert.assertEquals(Math.round(2.533), 3, "2.533 = 3");
+    }
+
+    @Test(groups = "round")
+    public void roundNumber2() {
+        System.out.println("roundNumber2");
+        Assert.assertEquals(Math.round(58.6), 3, "2.533 = 3");
     }
 
     @AfterMethod
     public void afterClassMethod() {
         System.out.println("After method");
     }
+
     @BeforeClass
     public void beforeClass() {
         System.out.println("beforeClass");
