831359e4587e821c8db01d1fdead3fec1d365ec0
Alena_Mi <lenalikestesting@gmail.com>
2020-12-06 23:27:54 +0300

    task-5-brainium-tests: in progress. Device cant be added, all done in login

--- alena_automationLessons/src/test/java/com/webDriver/BrainiumLogin.java
+++ alena_automationLessons/src/test/java/com/webDriver/BrainiumLogin.java
@@ -27,12 +27,27 @@ public class BrainiumLogin {
         Thread.sleep(5000);
         WebElement checkBox = driver.findElement(By.xpath("//div[@class='custom-icon left-sidebar__link-icon']"));
 
-        //create project
-        WebElement submitBtn2 = driver.findElement(By.xpath("//button[@class='btn btn--floating oct-h8']"));
-        submitBtn2.click();
+// create project
+        WebElement createProjectBtn1 = driver.findElement(By.xpath("//button[@class='btn btn--floating oct-h8']"));
+        createProjectBtn1.click();
         WebElement nameInput = driver.findElement(By.xpath("//input[@id=\"project-name\"]"));
-        emailInput.sendKeys("test");
+        nameInput.sendKeys("test");
+        WebElement createProjectBtn2 = driver.findElement(By.xpath("//span[text()=\"Create Project\"]/ancestor::button"));
+        createProjectBtn2.click();
+
+        //add device to project  -- not work -  element not interactable
+        WebElement addDeviceBtn = driver.findElement(By.xpath("//div[@class='btn--floating__label']"));
+        addDeviceBtn.click();
+        Thread.sleep(1000);
+        WebElement addToTheProjectBtn = driver.findElement(By.xpath("//button[@class='btn btn--contained oct-h9 btn--primary']"));
+        addToTheProjectBtn.click();
+
+        //add widget
+        WebElement projectsTab = driver.findElement(By.xpath("//button[@aria-controls='tabs--24--panel--0']"));
+        projectsTab.click();
+
 
 //        driver.quit();
     }
 }
+
