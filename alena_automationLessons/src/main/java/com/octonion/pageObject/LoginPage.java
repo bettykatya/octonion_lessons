f3b29334973dd89635797e120c79c2cf4083fe53
Alena_Mi <lenalikestesting@gmail.com>
2021-02-06 18:57:42 +0300

    task-6-am-pageObject

---
+++ alena_automationLessons/src/main/java/com/octonion/pageObject/LoginPage.java
@@ -0,0 +1,12 @@
+package com.octonion.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+}
