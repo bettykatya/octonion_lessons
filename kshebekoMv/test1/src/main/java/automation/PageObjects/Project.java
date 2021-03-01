3fc97e3aed6ef4e2e4b7a275d1a2e5866a7aeaeb
Katerina_Shebeko <katerinashebeko@gmail.com>
2021-03-01 12:37:08 +0300

    + delete project

--- kshebekoMv/test1/src/main/java/automation/PageObjects/Project.java
+++ kshebekoMv/test1/src/main/java/automation/PageObjects/Project.java
@@ -21,6 +21,21 @@ public class Project extends BasePageObjects {
     @FindBy(xpath = "//div[@class='custom-icon btn__icon']")
     private WebElement clickAddDevice;
 
+    @FindBy(xpath = "//span[contains(text(), 'Add to the Project')]")
+    private WebElement addToTheProject;
+
+    @FindBy(xpath = "//a[@aria-label = 'List of Projects']/div/div")
+    private WebElement listOfProjects;
+
+    @FindBy(xpath = "//div[@class = 'milfei-dimmable projects-page__cards-row'][1]/a[1]/child::*[2]")
+    private WebElement projectSetting;
+
+    @FindBy(xpath = "//span[contains(text(), 'Remove')]/..")
+    private WebElement projectRemoveBtn;
+
+    @FindBy(xpath = "//span[contains(text(), 'Confirm')]/..")
+    private WebElement confirmRemoveProject;
+
     public void clickCreateProject() { btnCreateProject.click();}
 
     public void inputName(String name) {inputProjectName.sendKeys(name);}
@@ -29,6 +44,15 @@ public class Project extends BasePageObjects {
 
     public void clickDevice() {clickAddDevice.click();}
 
+    public void btnAddDevice() {addToTheProject.click();}
+
+    public void clickListOfProjects() {listOfProjects.click();}
+
+    public void clickProjectSetting() {projectSetting.click();}
+
+    public void clickProjectRemove() {projectRemoveBtn.click();}
+
+    public void confirmRemoveProjectBtn() {confirmRemoveProject.click();}
 
     public Project(WebDriver driver) {
         this.driver = driver;
--- kshebekoMv/test1/src/test/java/PageObjectsTest/TestBrownie.java
+++ kshebekoMv/test1/src/test/java/PageObjectsTest/TestBrownie.java
@@ -17,22 +17,38 @@ public class TestBrownie extends BaseTest {
         login.enterPassword("qwertyQ1");
         login.submit();
         Thread.sleep(5000);
+        //todo Assert: new page
     }
 
     @Test(dependsOnMethods = "enterLoginData")
     public void createProject() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(5000); // todo wait
         project = new Project(driver);
         project.clickCreateProject();
         project.inputName("testKaty");
         project.btnCreate();
+        //todo Assert there is project new
     }
 
     @Test(dependsOnMethods = "createProject")
     public void addDeviceInProject() throws InterruptedException {
-        Thread.sleep(5000); //Wait
-        project = new Project(driver);
+        Thread.sleep(5000); //todo Wait
         project.clickDevice();
+        Thread.sleep(5000); //todo Wait
+        project.btnAddDevice();
+        //todo Assert add device
+    }
+
+    //todo: create widget device
+
+    @Test(dependsOnMethods = "addDeviceInProject")
+    public void deleteProject() throws InterruptedException {
+        project.clickListOfProjects();
+        Thread.sleep(5000); // todo wait
+        project.clickProjectSetting();
+        project.clickProjectRemove();
+        project.confirmRemoveProjectBtn();
+
     }
 
 
